9d7cec1b45dc7e5997167c355291a5cf04eb67b4
Alberto Rodriguez <ararod9@gmail.com>
2021-01-02 23:59:14 -0500

    fix exception

--- src/main/java/com/codedifferently/hurt/ItemParser.java
+++ src/main/java/com/codedifferently/hurt/ItemParser.java
@@ -1,9 +1,6 @@
 package com.codedifferently.hurt;
 
 
-import com.codedifferently.hurt.exceptions.ItemParserItemException;
-import com.codedifferently.hurt.exceptions.ItemParserMissingValueException;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -18,27 +15,23 @@ public class ItemParser {
 		listOfKey.add("expiration");
 	}
 
-	public Item convertStringToItem(String keyValuePairs) throws ItemParserItemException {
+	public Item convertStringToItem(String keyValuePairs) {
 		/*
 		1.get each value
 		2.insert value into Item object
-		3.exception if value is null
+		3.store error if value is null
 		 */
 		HashMap<String,String> itemData = new HashMap<>();
-		try {
+
 			for (String key:listOfKey) {
 				String value = findValueByKey(key,keyValuePairs);
 				itemData.put(key,value);
 			}
 
 			return new Item(itemData);
-		}catch (ItemParserMissingValueException e){
-			e.printStackTrace();
-			throw new ItemParserItemException();
-		}
 	}
 
-	public String findValueByKey(String key, String data)throws ItemParserMissingValueException {
+	public String findValueByKey(String key, String data){
 		/*
 		1.use data to find the key by matching value in data
 		2.when matched return non alter form of value and key
@@ -53,10 +46,15 @@ public class ItemParser {
 			String[] pairSplit = eachPair.split(":");
 			//grab each key
 			String currentPair = pairSplit[0];
-			//compare key to this key
-			if(key.equalsIgnoreCase(currentPair)){
-				if(pairSplit.length < 2) throw new ItemParserMissingValueException();
-				value = pairSplit[1];
+			//compare key to this key and throw exception when value is not present
+			try{
+				if(key.equalsIgnoreCase(currentPair)){
+					if(pairSplit.length < 2) throw new ItemParserMissingValueException();
+					value = pairSplit[1];
+				}
+			}catch (ItemParserMissingValueException valueException){
+				valueException.printStackTrace();
+				return value;
 			}
 		}
 		return value;
--- src/main/java/com/codedifferently/hurt/exceptions/ItemParserItemException.java
+++
@@ -1,4 +0,0 @@
-package com.codedifferently.hurt.exceptions;
-
-public class ItemParserItemException extends Exception{
-}

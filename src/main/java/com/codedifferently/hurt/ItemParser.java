66ef279cd028b3a7486dfd12cb68726a1a533867
Alberto Rodriguez <ararod9@gmail.com>
2021-01-02 23:19:04 -0500

    working on exception handling

--- src/main/java/com/codedifferently/hurt/ItemParser.java
+++ src/main/java/com/codedifferently/hurt/ItemParser.java
@@ -1,19 +1,44 @@
 package com.codedifferently.hurt;
 
 
-public class ItemParser {
+import com.codedifferently.hurt.exceptions.ItemParserItemException;
+import com.codedifferently.hurt.exceptions.ItemParserMissingValueException;
 
-	public Item convertStringToItem(String keyValuePair){
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ItemParser {
+	private ArrayList<String > listOfKey;
+
+	public ItemParser(){
+		listOfKey = new ArrayList<>();
+		listOfKey.add("name");
+		listOfKey.add("price");
+		listOfKey.add("item");
+		listOfKey.add("expiration");
+	}
+
+	public Item convertStringToItem(String keyValuePairs) throws ItemParserItemException {
 		/*
 		1.get each value
 		2.insert value into Item object
 		3.exception if value is null
 		 */
+		HashMap<String,String> itemData = new HashMap<>();
+		try {
+			for (String key:listOfKey) {
+				String value = findValueByKey(key,keyValuePairs);
+				itemData.put(key,value);
+			}
 
-		return null;
+			return new Item(itemData);
+		}catch (ItemParserMissingValueException e){
+			e.printStackTrace();
+			throw new ItemParserItemException();
+		}
 	}
 
-	public String findValueByKey(String key, String data){
+	public String findValueByKey(String key, String data)throws ItemParserMissingValueException {
 		/*
 		1.use data to find the key by matching value in data
 		2.when matched return non alter form of value and key
@@ -22,7 +47,7 @@ public class ItemParser {
 		//replace non related special character as spaces
 		data = data.replaceAll("[^A-Za-z0-9#/:.]"," ");
 		String[] base = data.split("\\s");
-		String keyValuePair = "";
+		String value = null;
 		for (String eachPair : base) {
 			//separate key from value
 			String[] pairSplit = eachPair.split(":");
@@ -30,10 +55,10 @@ public class ItemParser {
 			String currentPair = pairSplit[0];
 			//compare key to this key
 			if(key.equalsIgnoreCase(currentPair)){
-				keyValuePair=eachPair;
+				if(pairSplit.length < 2) throw new ItemParserMissingValueException();
+				value = pairSplit[1];
 			}
 		}
-
-		return keyValuePair;
+		return value;
 	}
 }

32a68690ca18daca59c1367e92f56448d6ac2d2d
stainlessray <raymondcoolmail@gmail.com>
2021-01-04 10:18:43 -0500

    bread output format complete

--- src/main/java/com/codedifferently/hurt/chartBuilder.java
+++ src/main/java/com/codedifferently/hurt/chartBuilder.java
@@ -62,7 +62,7 @@ public class chartBuilder {
                 String name = line1[0].trim();
                 String priceName = line2[0].trim();
 
-                assemblerString = String.format("%s  : %s" + spacer + "seen: %d times", StringUtils.center(name, 5), StringUtils.center(line1[1].trim(), 4), milkCount);
+                assemblerString = String.format("\n%s  : %s" + spacer + "seen: %d times", StringUtils.center(name, 5), StringUtils.center(line1[1].trim(), 4), milkCount);
                 System.out.println(assemblerString + assemblerString.length());
 
                 String header = StringUtils.center(StringUtils.center(headerLine, 13) + StringUtils.center(spacer, 8) + StringUtils.center(headerLine, 13), 34);
@@ -91,6 +91,54 @@ public class chartBuilder {
     }
 
     public String createBreadChart(ArrayList bread) {
+        String[] tempStrArray = new String[bread.size()];
+        String[] line1 = new String[bread.size()];
+        String[] line2 = new String[bread.size()];
+
+        String tempString = "";
+        String tempString2 = "";
+        String assemblerString = "";
+        String assemblerString2 = "";
+        String assemblerString3 = "";
+        String assemblerString4 = "";
+
+        for (int i =0; i < bread.size(); i++) {
+            if (i == 0) {
+                tempStrArray = bread.get(i).toString().split(",");
+                tempString = String.format(tempStrArray[0]);
+                tempString2 = String.format(tempStrArray[1]);
+                String priceCount = tempStrArray[2].replace("=", "");
+
+                line1 = tempString.split(":");
+                line2 = tempString2.split(":");
+                String name = line1[0].trim();
+                String priceName = line2[0].trim();
+
+                assemblerString = String.format("\n%s  :%s" + spacer + "seen: %d times", StringUtils.center(name, 5), StringUtils.center(line1[1].trim(), 4), milkCount);
+                System.out.println(assemblerString + assemblerString.length());
+
+                String header = StringUtils.center(StringUtils.center(headerLine, 13) + StringUtils.center(spacer, 8) + StringUtils.center(headerLine, 13), 34);
+                System.out.println(header + header.length());
+
+
+                assemblerString2 = String.format("%s : %s" + spacer + "seen: %s times", StringUtils.center(priceName, 6), StringUtils.center(line2[1].trim(), 4), priceCount);
+
+                String rowLine = StringUtils.center(rowDivider + spacer + rowDivider, 32);
+                System.out.println(assemblerString2 + assemblerString2.length());
+                System.out.println(rowLine);
+            }
+
+            if (i > 0 && i < bread.size()) {
+                tempStrArray = bread.get(i).toString().split(",");
+                tempString2 = String.format(tempStrArray[1]);
+                String priceCount = tempStrArray[2].replace("=", "");
+
+                line2 = tempString2.split(":");
+
+                assemblerString4 = String.format("%s : %s" + spacer + "seen: %s times", StringUtils.center(line2[0], 6), StringUtils.center(line2[1], 4), priceCount);
+                System.out.println(assemblerString4 + assemblerString4.length());
+            }
+        }
         return null;
     }
 

8d230c060ee86a213688a39fe90d322bd74a4b0f
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-04 17:16:16 -0500

    tried to test for price to in productParserTest class but still its failing

--- src/test/java/com/codedifferently/hurt/ProductParserTest.java
+++ src/test/java/com/codedifferently/hurt/ProductParserTest.java
@@ -6,11 +6,20 @@ import org.junit.Test;
 public class ProductParserTest {
     private String sampleItem= "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016";
     @Test
-    public void findFieldByKeyValue(){
+    public void findFieldByKeyValueTest1(){
         ProductParser productParser =new ProductParser();
 
-        String expected ="naMe:Milk";
+        String expected ="Milk";
         String actual = productParser.findFieldByKeyValue("naMe",sampleItem);
         Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void findFieldByKeyValueTest2(){
+        ProductParser productParser =new ProductParser();
+
+        Double expected =3.23;
+        Double actual = Double.parseDouble(productParser.findFieldByKeyValue("price",sampleItem));
+        Assert.assertEquals(expected,actual,0.00);
     }
 }

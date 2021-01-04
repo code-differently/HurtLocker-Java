a4c31a8cd7b3bd9749109e960eb42182360e572c
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-04 17:03:45 -0500

    worked on productparse class

--- src/main/java/com/codedifferently/hurt/ProductParser.java
+++ src/main/java/com/codedifferently/hurt/ProductParser.java
@@ -2,6 +2,18 @@ package com.codedifferently.hurt;
 
 public class ProductParser {
     public String findFieldByKeyValue(String key,String data){
-        return null;
+        //  split the data into an array
+        String[] dataArray = data.split("[^0-9a-zA-Z#./;]+");
+        //search array for key.
+        String value = null;
+        for(String product:dataArray){
+            if(product.contains(key)){
+                String[] productArray = product.split(":");
+                value=productArray[1];
+            }
+        }
+        //if we find key split string by :
+        //save to return value at index 1
+        return value;
     }
 }

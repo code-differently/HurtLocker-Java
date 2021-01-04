f543e29244291fdb0544996df028a66c919c65c3
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-04 15:32:29 -0500

    Worked on the product class creating variables with datatypes,constructor and getter and setter. Created the product class where all the main work can be done

--- src/main/java/com/codedifferently/hurt/Product.java
+++ src/main/java/com/codedifferently/hurt/Product.java
@@ -1,4 +1,47 @@
 package com.codedifferently.hurt;
 
 public class Product {
+    private String name;
+    private Double price;
+    private String type;
+    private String expiration;
+
+    public Product(String name, Double price, String type, String expiration) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.expiration = expiration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(String expiration) {
+        this.expiration = expiration;
+    }
 }

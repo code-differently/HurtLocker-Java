8525c27eb26a10f17411e6b92a6a39d2c59e2677
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-04 16:29:50 -0500

    Worked on the ProductParser class and created a productParser test class

--- src/main/java/com/codedifferently/hurt/ProductParser.java
+++ src/main/java/com/codedifferently/hurt/ProductParser.java
@@ -1,4 +1,7 @@
 package com.codedifferently.hurt;
 
 public class ProductParser {
+    public String findFieldByKeyValue(String key,String data){
+        return null;
+    }
 }
---
+++ src/test/java/com/codedifferently/hurt/ProductParserTest.java
@@ -0,0 +1,11 @@
+package com.codedifferently.hurt;
+
+import org.junit.Test;
+
+public class ProductParserTest {
+    private String sampleItem= "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016";
+    @Test
+    public void findFieldByKeyValue(){
+        ProductParser productParser =new ProductParser();
+    }
+}
